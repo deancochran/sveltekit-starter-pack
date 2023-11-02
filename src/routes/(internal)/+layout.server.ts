6a016e3ea0870d0d1f76c7301cb41064f1f747b2
Dean Cochran <charles.dean.cochran@outlook.com>
2023-11-02 14:22:33 -0500

    handleLoginRedirect

--- src/routes/(internal)/+layout.server.ts
+++ src/routes/(internal)/+layout.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
 import type { LayoutServerLoad } from './$types';
+import { handleSignInRedirect } from '$lib/utils/redirects/loginRedirect';
 
 export const load: LayoutServerLoad = async (event) => {
 	const { parent } = event;
 	const data = await parent();
-	if (!data.session) throw redirect(302, '/sign-in');
+	if (!data.session) throw redirect(302, handleSignInRedirect(event));
 
 	return data;
 };
